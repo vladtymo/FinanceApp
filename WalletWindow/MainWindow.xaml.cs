3f14e2e92b4f39dcf4871e7e8d0130c4582142a8
Paliy Igop <Igop2828@gmail.com>
2021-04-09 09:06:39 +0300

    Left border and theme

---
+++ WalletWindow/MainWindow.xaml.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using WalletWindow.Themes;
+
+namespace WalletWindow
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //static bool checkMenu = true; 
+        public string CurrentDate { get; set; }
+        private DispatcherTimer timer;
+        double panelWidth;
+        bool hidden;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            CurrentDate = DateTime.Now.ToShortDateString().ToString();
+
+            this.DataContext = this;
+
+            timer = new DispatcherTimer();
+            timer.Interval = new TimeSpan(0, 0, 0, 0, 10);
+            timer.Tick += Timer_Tick;
+
+            panelWidth = sidePanel.Width;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if (hidden)
+            {
+                sidePanel.Width += 5;
+                if (sidePanel.Width >= panelWidth)
+                {
+                    timer.Stop();
+                    hidden = false;
+                }
+            }
+            else
+            {
+                sidePanel.Width -= 5;
+                if (sidePanel.Width <= 45)
+                {
+                    timer.Stop();
+                    hidden = true;
+                }
+            }
+        }
+
+        private void ChangeTheme(object sender, RoutedEventArgs e)
+        {
+            switch (int.Parse(((Button)sender).Uid))
+            {
+                case 0: ThemesController.SetTheme(ThemesController.ThemeTypes.Light); break;
+                case 1: ThemesController.SetTheme(ThemesController.ThemeTypes.ColourfulLight); break;
+                case 2: ThemesController.SetTheme(ThemesController.ThemeTypes.Dark); break;
+                case 3: ThemesController.SetTheme(ThemesController.ThemeTypes.ColourfulDark); break;
+            }
+            e.Handled = true;
+        }
+
+        private void btnClickMenu(object sender, RoutedEventArgs e)
+        {
+            timer.Start();
+        }
+
+        private void PanelHeader_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+        private void btnChangeTheme(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        //private void Button_Click(object sender, RoutedEventArgs e)
+        //{
+
+        //}
+
+        //private void buttonMenuClick(object sender, RoutedEventArgs e)
+        //{
+        //    if (checkMenu)
+        //    {
+        //        borderMain.Width = this.Width - groupBox.Width;
+        //        checkMenu = false;
+        //    }
+        //    else
+        //    {
+        //        borderMain.Width = this.Width + groupBox.Width;
+        //        checkMenu = true;
+        //    }
+
+        //}
+    }
+}
